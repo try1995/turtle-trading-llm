693e2e671e9694777194abdb03e43fdc3d49f918
try1995 <1635341612@qq.com>
2026-01-21 21:32:18 +0800

    update qinglong脚本

--- qionglong.py
+++ qionglong.py
@@ -22,23 +22,47 @@ import sys
 logger.remove()                                     # 去掉默认全局配置
 logger.add(sys.stderr, level="INFO") 
 
+exclude_symbol = os.environ.get("exclude_symbol").split("|")
+position_symbol = os.environ.get("position_symbol").split("|")
 
-def daily_task():
-    now = datetime.now().strftime("%Y%m%d")
-    if now not in get_trade_date():
-        logger.info("未在交易日，跳过")
-        return
+# 热榜选股
+def hot_symbol_task():
     stock_hot_deal_xq_df = ak.stock_hot_deal_xq(symbol="最热门")
     # 人气榜-A股
     stock_hot_rank_em_df = ak.stock_hot_rank_em()
     
     df = pd.merge(stock_hot_deal_xq_df.head(200), stock_hot_rank_em_df.head(200), how='inner')
     
-    # 茅台太贵
-    exclude_symbol = os.environ.get("exclude_symbol").split("|")
-    # 持仓股票
-    position_symbol = os.environ.get("position_symbol").split("|")
+    logger.info(df)
+    
+                
+    for _, item in df.iterrows():
+        symbol = item.股票代码[2:]
+        stock_info = ak.stock_individual_info_em(symbol)
+        stock_info_dict = stock_info.set_index('item')['value'].to_dict()
+        if stock_info_dict["总市值"] < 800 * 100000000:  # 市值大于一千亿
+            continue
+        if symbol.startswith("3"):
+            continue
+        if symbol in exclude_symbol:
+            continue
+        if symbol in position_symbol:
+            continue
+        logger.info(stock_info)
+        plan = PlanAgent()
+        plan.set_symbol(symbol)
+        maxretry = 3
+        while maxretry:
+            try:
+                plan.run(f"详细分析{symbol}行情情况，提供交易建议", human_in_loop=False)
+                plan.send_allres_email(subject=f"{item.股票简称}分析")
+                break
+            except Exception as e:
+                logger.error(e)
+                maxretry -= 1
 
+# 持仓股票
+def position_symbol_task():
     for symbol in position_symbol:
         plan = PlanAgent()
         plan.set_symbol(symbol)
@@ -51,31 +75,15 @@ def daily_task():
             except Exception as e:
                 logger.error(e)
                 maxretry -= 1
-                
-    # for _, item in df.iterrows():
-    #     symbol = item.股票代码[2:]
-    #     stock_info = ak.stock_individual_info_em(symbol)
-    #     stock_info_dict = stock_info.set_index('item')['value'].to_dict()
-    #     if stock_info_dict["总市值"] < 800 * 100000000:  # 市值大于一千亿
-    #         continue
-    #     if symbol.startswith("3"):
-    #         continue
-    #     if symbol in exclude_symbol:
-    #         continue
-    #     if symbol in position_symbol:
-    #         continue
-    #     print(stock_info)
-    #     plan = PlanAgent()
-    #     plan.set_symbol(symbol)
-    #     maxretry = 3
-    #     while maxretry:
-    #         try:
-    #             plan.run(f"详细分析{symbol}行情情况，提供交易建议", human_in_loop=False)
-    #             plan.send_allres_email(subject=f"{item.股票简称}分析")
-    #             break
-    #         except Exception as e:
-    #             logger.error(e)
-    #             maxretry -= 1
+
+def daily_task():
+    now = datetime.now().strftime("%Y%m%d")
+    if now not in get_trade_date():
+        logger.info("未在交易日，跳过")
+        return
+    position_symbol_task()
+    hot_symbol_task()
+    
     
 
 
