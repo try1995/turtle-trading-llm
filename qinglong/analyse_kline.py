8a89763f25502ffbf072675f9a66952e6b65458e
try1995 <1635341612@qq.com>
2026-03-17 21:59:13 +0800

    添加租户控制

--- qinglong/analyse_kline.py
+++ qinglong/analyse_kline.py
@@ -12,7 +12,7 @@ import pandas as pd
 import pandas as pd
 from tools import get_trade_date, get_a_symbol_info
 from datetime import datetime
-
+from tools.all_types import Tenant
 
 from agents.vlAgent import VlAgent
 from loguru import logger
@@ -20,9 +20,7 @@ from loguru import logger
 logger.remove()                                     # 去掉默认全局配置
 logger.add(sys.stderr, level="INFO") 
 
-exclude_symbol = os.environ.get("exclude_symbol", "").split("|")
-position_symbol = os.environ.get("position_symbol", "").split("|")
-toaddrs = os.environ.get("toaddrs", "").split("|")
+
 # 热榜选股
 def analysis_task():
     vl_agent = VlAgent()
@@ -40,4 +38,23 @@ def daily_task():
     
     
 if __name__ == "__main__":
-    daily_task()
+    # python analyse_kline.py '{"name":"汤总","toaddrs":"1635341612@qq.com","exclude_symbol":"000001","position_symbol":"601601"}'
+    # 判断是否传入参数
+    if len(sys.argv) > 1:
+        arg1 = sys.argv[1]
+        logger.info(f"tenant参数：{arg1}")
+
+        tenant_raw = os.environ.get(arg1, "")
+        
+        if tenant_raw:
+            tenant = Tenant.parse_raw(arg1)
+            dear = tenant.name
+            toaddrs = tenant.toaddrs.split("|")
+            exclude_symbol = tenant.exclude_symbol.split("|")
+            position_symbol = tenant.position_symbol.split("|")      
+
+            daily_task()
+        else:
+            logger.error(f"未定义环境变量{arg1}")
+    else:
+        logger.error("未定义tenant")
