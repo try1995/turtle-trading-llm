f82e0e17c0963da4a9b7c9d37df8652fe13bdd49
try1995 <1635341612@qq.com>
2026-04-12 14:38:15 +0800

    refactor hot_symbol_task to include tenant handling and email parameters

--- qinglong/hot_symbol.py
+++ qinglong/hot_symbol.py
@@ -14,16 +14,13 @@ from tools import get_trade_date, get_a_symbol_info
 from datetime import datetime
 from qinglong.daily_stock_analysis import analysis_stock
 
-
+from tools.all_types import Tenant
 from agents.baseAgent import baseAgent
 from agents.planAgent import PlanAgent
 from loguru import logger
 
 logger.remove()                                     # 去掉默认全局配置
 logger.add(sys.stderr, level="INFO") 
-
-exclude_symbol = os.environ.get("exclude_symbol", "").split("|")
-position_symbol = os.environ.get("position_symbol", "").split("|")
 
 # 热榜选股
 def hot_symbol_task():
@@ -43,7 +40,7 @@ def hot_symbol_task():
         email_df['涨跌幅'] = email_df['涨跌幅'].apply(
             lambda x: f'<span style="color: green;">{x}</span>' if x < 0 else f'<span style="color: red;">{x}</span>'
         )
-        plan.send_res_email(email_df.to_markdown(index=False), subject="每日热榜", table=True)
+        plan.send_res_email(email_df.to_markdown(index=False), subject="每日热榜", table=True, toaddrs=toaddrs, dear=dear)
     
     for _, item in df.iterrows():
         symbol = item.代码[2:]
@@ -59,4 +56,22 @@ def daily_task():
     
     
 if __name__ == "__main__":
-    daily_task()
+    if len(sys.argv) > 1:
+        arg1 = sys.argv[1]
+        logger.info(f"tenant参数：{arg1}")
+
+        tenant_raw = os.environ.get(arg1, "")
+        
+        if tenant_raw:
+            tenant = Tenant.model_validate_json(tenant_raw)
+            dear = tenant.name
+            toaddrs = tenant.toaddrs.split("|")
+            exclude_symbol = tenant.exclude_symbol.split("|")
+            position_symbol = tenant.position_symbol.split("|")      
+
+            daily_task()
+        else:
+            logger.error(f"未定义环境变量{arg1}")
+    else:
+        logger.error("未定义tenant")
+
